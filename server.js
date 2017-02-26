1f18b55518e96bb8b3ae69b8fac30c13e16d1ff0
AntoinePorcheron <antoine.pr@live.fr>
2017-02-26 16:05:16 +0100

    try to update some parts

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ var builder = require('botbuilder');
 
 var server = restify.createServer();
 
-/*console.log(process.env.PORT)
-console.log(*/
-server.listen(process.env.port || process.env.PORT || 3978, function () {
+server.listen(process.env.port || process.env.PORT || 80, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
   
 var connector = new builder.ChatConnector({
-    appId: /*process.env.MICROSOFT_APP_ID*/5fab6329-744a-45ad-9259-3767e61a39d1,
-    appPassword: /*process.env.MICROSOFT_APP_PASSWORD*/8orDy9Xsc0O2mHY0jTkhVmH
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 var bot = new builder.UniversalBot(connector);
